ee5884b6ec5c5e96da82b80e2279632ad546319e
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-20 13:53:11 +0530

    use res.write to send multiple line

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const app = express();
 app.get("/",function(req,res){
     //res.send("Hello");
     var today = new Date();
-    if(today.getDay() === 6 || today.getDay() === 0)  {
-        res.send("Yay it is weekend!");
+    var currentDay = today.getDay();
+    if(currentDay === 6 || currentDay === 0)  {
+        res.write("<h1>Yay it is weekend!</h1>");
     } else {
-        res.send("Boo! I have to work!");
+        res.write("<p>It is not the weekend</p>");
+
+        res.write("<h1>Boo! I have to work!</h1>");
     }
+    res.send();
 });
 
 app.listen(3000,function(){
