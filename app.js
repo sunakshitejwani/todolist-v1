82a3174227e44c87bc38750637e31476aa08eba6
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-20 15:11:54 +0530

    use ejs template to display weekday

--- app.js
+++ app.js
@@ -3,21 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/",function(req,res){
-    //res.send("Hello");
-    var today = new Date();
-    var currentDay = today.getDay();
-    if(currentDay === 6 || currentDay === 0)  {
-        res.write("<h1>Yay it is weekend!</h1>");
-    } else {
-        // res.write("<p>It is not the weekend</p>");
+app.set("view engine", "ejs");
 
-        // res.write("<h1>Boo! I have to work!</h1>");
-        res.sendFile(__dirname + "/index.html");
-    }
-    //res.send();
+app.get("/", function(req, res) {
+  //res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+  if (currentDay === 0) {
+    day = "Sunday";
+  } else if (currentDay === 1) {
+    day = "Monday";
+  } else if (currentDay === 2) {
+    day = "Tuesday";
+  } else if (currentDay === 3) {
+    day = "Wednesday";
+  } else if (currentDay === 4) {
+    day = "Thursday";
+  } else if (currentDay === 5) {
+    day = "Friday";
+  } else if (currentDay === 6) {
+    day = "Saturday";
+  }
+  res.render("list", { kindofDay: day });
 });
 
-app.listen(3000,function(){
-    console.log("server is running on port 3000");
-})
+app.listen(3000, function() {
+  console.log("server is running on port 3000");
+});
