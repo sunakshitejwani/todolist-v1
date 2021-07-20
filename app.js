bc150c1f4b5b6f99f060538a3006306c5bae379d
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-20 20:02:36 +0530

    pass data from web page to server

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var item = "";
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res) {
   //res.send("Hello");
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  if (currentDay === 0) {
-    day = "Sunday";
-  } else if (currentDay === 1) {
-    day = "Monday";
-  } else if (currentDay === 2) {
-    day = "Tuesday";
-  } else if (currentDay === 3) {
-    day = "Wednesday";
-  } else if (currentDay === 4) {
-    day = "Thursday";
-  } else if (currentDay === 5) {
-    day = "Friday";
-  } else if (currentDay === 6) {
-    day = "Saturday";
+  var options = {
+      weekday:"long",
+      day:"numeric",
+      month:"long"
   }
-  res.render("list", { kindofDay: day });
+
+  var day = today.toLocaleDateString("en-US",options);
+
+  res.render("list", { kindofDay: day, newListItem:item });
 });
+
+app.post("/",function(req,res){
+    item = req.body.newItem;
+    //res.render("list",{newListItem:item});
+    res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("server is running on port 3000");
