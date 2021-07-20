f73a1b971079ac92f6aaf42252758a05db4bfdf8
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-20 23:02:08 +0530

    implement templates and layouts

--- app.js
+++ app.js
@@ -3,31 +3,52 @@ const bodyParser = require("body-parser");
 
 const app = express();
 var items = [];
+var workItems = [];
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   //res.send("Hello");
   var today = new Date();
   var options = {
-      weekday:"long",
-      day:"numeric",
-      month:"long"
-  }
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  var day = today.toLocaleDateString("en-US",options);
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindofDay: day, newListItems:items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
-app.post("/",function(req,res){
-    var item = req.body.newItem;
-    //res.render("list",{newListItem:item});
-    items.push(item)
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
     res.redirect("/");
+  }
+  //res.render("list",{newListItem:item});
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
 })
 
 app.listen(3000, function() {
