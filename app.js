bb9b9cf2682ea3daec253b0aa357060c8dd6e58f
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-25 12:55:07 +0530

    add new items to our database

--- app.js
+++ app.js
@@ -59,16 +59,16 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-  //res.render("list",{newListItem:item});
+  const item = new Item({
+    name:itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
+
 });
 
 app.get("/work", function(req, res) {
