29cbfb66c9773649440721fc0d5b810acea552b1
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-25 14:50:03 +0530

    revisit lodash to fix case of begin letter of list title

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const app = express();
 var items = [];
 var workItems = [];
@@ -104,7 +105,7 @@ app.post("/delete", function(req, res) {
 });
 
 app.get("/:customListName", function(req, res) {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName }, function(err, foundList) {
     if (!err) {
